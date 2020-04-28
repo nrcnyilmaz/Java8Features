edf017f52906901999a9ce5a11060807f6a276ec
nrcnyilmaz <nyilmaz@uni-yaz.com>
2020-04-28 13:21:14 +0300

    düzenleme

--- src/main/java/functionalinterface/FuncInterfaceMain.java
+++ src/main/java/functionalinterface/FuncInterfaceMain.java
@@ -15,12 +15,12 @@ public class FuncInterfaceMain {
         // f(x,y) = (3*x + 2*y)/5
         Equation equation1 = (x, y) -> (3 * x + 2 * y) / 5;
         double calculate = equation1.calculate(11, 9);
-        System.out.println("(3*x + 2*y)/5 funtion result is = " + calculate);
+        System.out.println("(3*x + 2*y)/5 function result is = " + calculate);
 
         // f(x,y) = sqrt(x-1) - y
         Equation equation2 = (x, y) -> Math.sqrt(x - 1) - y;
         double calculate2 = equation2.calculate(26, 8);
-        System.out.println("(sqrt(x-1)) - y funtion result is = " + calculate2);
+        System.out.println("(sqrt(x-1)) - y function result is = " + calculate2);
 
         MathProcess mathProcess = new MathProcess();
         int valueAddOne = mathProcess.addOne((x, y) -> x * y, 1, 2);
